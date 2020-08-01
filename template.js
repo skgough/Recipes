dfde5fb739dcb7c6f42c023715d9ff99e2f15952
Stephen <skgough@gmail.com>
2020-08-01 12:19:03 -0500

    template rework

--- template.js
+++ template.js
@@ -1,217 +1,69 @@
-class Editable {
-    // wraps the element passed as argument in a class and attaches event listeners
-    constructor(element) {
-        let wrapper = this;                                 // bind sets *this* context to the passed element
-        element.addEventListener('blur',   wrapper.blurHandler.bind(element, wrapper));
-        element.addEventListener('focus', wrapper.focusHandler.bind(element, wrapper));
-        element.addEventListener('input', wrapper.inputHandler.bind(element, wrapper));
-
-        // element is available as Editable.element
-        this.element = element;
-    }
-
-    // if text hasn't been edited, clear out text (perceived behaviour)
-    focusHandler(wrapper) {
-        // wrapper.edited is set after input
-        if (!wrapper.edited) {
-            // save the element's current text as wrapper.previous
-            wrapper.previous = this.innerText;
-
-            // toggle on cefocused class, which hides text 
-            // but does not edit the html
-            this.classList.add('cefocused');
-
-            // collect the text nodes of element in a range, 
-            // and collapse them to zero length
-            let range = document.createRange();
-            range.setStart(this.childNodes[0], this.childNodes[0].length);
-            range.collapse(true);
-
-            // get current cursor position and clear selected text
-            let sel = window.getSelection();
-            sel.removeAllRanges();
-
-            /*  
-                set range as currently selected text.
-                this has the effect of setting the cursor 
-                position to the end of the text node.
-
-                this prevents the user from inserting text into
-                the middle of the template text which would
-                interfere with the inputHandler() function.
-            */
-            sel.addRange(range);
+let editables = document.querySelectorAll('[contenteditable="true"]');
+for (i = 0; i < editables.length; i++) {
+    editables[i].dataset.initialValue = editables[i].innerText;
+    editables[i].addEventListener('focus', function () {
+        if (this.innerText === this.dataset.initialValue) {
+            selectText(this)
         }
-    }
-
-
-    inputHandler(wrapper) {
-        if (!wrapper.edited) {
-            // subtract template text leaving only input
-            this.innerHTML = this.textContent.replace(wrapper.previous, '');
-
-            // set cursor to end
-            let range = document.createRange();
-            range.setStart(this.childNodes[0], this.childNodes[0].length);
-            range.collapse(true);
-            let sel = window.getSelection();
-            sel.removeAllRanges();
-            sel.addRange(range);
-
-            // remove cefocused so you can see the text
-            this.classList.remove('cefocused');
-        }
-        wrapper.edited = true;
-    }
-
-    // on defocus check if left blank and reset to template text
-    blurHandler(wrapper) {
+    });
+    editables[i].addEventListener('blur', function () {
         if (this.innerText === '') {
-            this.innerText = wrapper.previous;
-            wrapper.edited = false;
+            this.innerText = this.dataset.initialValue;
         }
-        this.classList.remove('cefocused');
-    }
-};
-
-// all button.delete elements delete their parent when clicked.
-class Deletor {
-    constructor(element) {
-        element.addEventListener('click', function () {
-            this.parentNode.remove()
-        });
-        this.element = element;
-    }
-};
-
-// arrays to contain all class members
-// this makes it easy to remove all dynamic content
-// during publish() through looping
-var editables = [];
-var deletors = [];
-
-window.onload = function () {
-    // all elements set to editable are collected as class Editable members
-    let editableElements = document.querySelectorAll('[contenteditable=true]');
-    for (i = 0; i < editableElements.length; i++) {
-        editables[i] = new Editable(editableElements[i]);
-    }
-
-    // collect delete buttons
-    let deletorElements = document.querySelectorAll('.delete');
-    for (i = 0; i < deletorElements.length; i++) {
-        deletors[i] = new Deletor(deletorElements[i])
-    }
-
-    // attach event listener to set doc title to the header text on input
-    document.querySelector('h1').addEventListener('input', function () {
-        if (document.title != this.textContent) {
-            document.title = this.textContent
-        }
-    });
-
-    // trigger hidden file input element on click.
-    // why? more attractive looking than the native file input element
-    document.querySelector('.filePicker').addEventListener('click', function () {
-        document.querySelector('input').click();
-    });
-
-    // loads the input image into the #addImage container
-    document.querySelector('input').addEventListener('change', handleFiles);
-
-    // clears selected image from #addImage
-    document.querySelector('button.cancel').addEventListener('click', function () {
-        document.querySelector('#addImage').lastChild.remove();
-        document.querySelector('#addImage').classList.remove('added');
-        document.querySelector('input').value = null;
-    });
-
-    // creates a new li in the ingredients column and makes it an Editable
-    document.querySelector('#addNewIngredient').addEventListener('click', addNewIngredient);
-
-    // creates a new p in the instructions and makes it an Editable
-    document.querySelector('#addNewStep').addEventListener('click', addNewStep);
-
-    // removes all scripted content and calls window.print()
-    document.querySelector('button.publish').addEventListener('click', publish);
-};
-
-// loads the input image into the #addImage container
-var handleFiles = function () {
-    let file = document.querySelector('input').files[0];
-    let reader = new FileReader();
-    if (file) {
-        reader.readAsDataURL(file);
-    }
-    reader.addEventListener("load", function () {
-        let image = document.createElement('img')
-        image.src = this.result;
-        document.querySelector('#addImage').appendChild(image);
-    });
-    document.querySelector('#addImage').classList.add('added');
-};
-
-
-
-// begin helper functions
-
-// creates a new li in the ingredients column and makes it an Editable
-var addNewIngredient = function () {
-    let newIngredient = document.createElement("li");
-    newIngredient.innerHTML = `<span contenteditable='true' class='quantity'>amount</span> \
-                               <span contenteditable='true'>ingredient</span> \
-                               <button title='delete item' class='delete'>&#10005;</button>`;
-    document.querySelector('ul').insertBefore(newIngredient, document.querySelector('#addNewIngredient'));
-    editables.push(new Editable(newIngredient.childNodes[0]));
-    editables.push(new Editable(newIngredient.childNodes[2]));
-    deletors.push(new Deletor(newIngredient.childNodes[4]));
-};
-
-// creates a new p in the instructions and makes it an Editable
-var addNewStep = function () {
-    let count = document.querySelector('div.preparation').childElementCount;
-    let newStep = document.createElement("p");
-    newStep.innerHTML = `<span contenteditable='true'>Do thing ${count}</span> \
-                         <button title='delete item' class='delete'>&#10005;</button>`;
-    document.querySelector('div.preparation').insertBefore(newStep, document.querySelector('#addNewStep'), newStep);
-    editables.push(new Editable(newStep.childNodes[0]));
-    deletors.push(new Deletor(newStep.childNodes[2]));
+        window.getSelection().removeAllRanges();
+    })
 }
 
-// removes all scripted content and calls window.print()
-var publish = function () {
-    let image = document.querySelector('img');
-    if (image === null) {
-        document.querySelector('#addImage').remove()
-    } else {
-        document.querySelector('#addImage').replaceWith(image)
-    }
+let addImageButton = document.querySelector('.flex > button');
+addImageButton.addEventListener('click', function () {
+    document.querySelector('input[type="file"]').click();
+});
+
+let addIngredientButton = document.querySelector('.ingredients tr:last-child button');
+addIngredientButton.addEventListener('click', function () {
+    let tr = document.createElement('tr');
+    tr.innerHTML = `<td><span contenteditable="true">Quantity</span></td>
+                        <td><span contenteditable="true">Ingredient</span></td>
+                        <td><button title="delete" class='delete' onclick='deleteRow(this)'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path fill="none" d="M0 0h24v24H0z"/>
+                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                      </svg></button></td>`;
+    let parent = document.querySelector('table.ingredients tbody');
+    let reference = parent.querySelector('tr:last-child');
+    parent.insertBefore(tr, reference);
+
+    let editables = tr.querySelectorAll('[contenteditable="true"]');
     for (i = 0; i < editables.length; i++) {
-        editables[i].element.removeAttribute('contenteditable');
-        let newNode = editables[i].element.cloneNode(true);
-        editables[i].element.parentNode.replaceChild(newNode, editables[i].element)
+        editables[i].dataset.initialValue = editables[i].innerText;
+        editables[i].addEventListener('focus', function () {
+            if (this.innerText === this.dataset.initialValue) {
+                selectText(this)
+            }
+        });
+        editables[i].addEventListener('blur', function () {
+            if (this.innerText === '') {
+                this.innerText = this.dataset.initialValue;
+            }
+            window.getSelection().removeAllRanges();
+        })
     }
-    for (i = 0; i < deletors.length; i++) {
-        deletors[i].element.remove();
-    }
-    document.querySelector('#addNewStep').remove();
-    document.querySelector('#addNewIngredient').remove();
+});
 
-    let quants = document.querySelectorAll('.quantity');
-    let width = 0;
-    for (i = 0; i < quants.length; i++) {
-        if (quants[i].offsetWidth > width) width = quants[i].offsetWidth;
+function selectText(node) {
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(node);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else {
+        console.warn("Could not select text in node: Unsupported browser.");
     }
-    for (i = 0; i < quants.length; i++) {
-        quants[i].style.width = width + 'px';
-        quants[i].parentElement.style.paddingLeft = width + 5 + 'px';
-        quants[i].parentElement.style.textIndent = -width - 5 + 'px';
-        quants[i].parentElement.innerHTML = quants[i].outerHTML + quants[i].nextElementSibling.innerText;
-    }
-    document.querySelector('a.home').remove();
-    document.querySelector('button.publish').remove();
-    document.head.querySelector('script').remove();
-    document.head.querySelector('[href="template.css"').remove();
-
-    window.print();
+}
+function deleteRow(node) {
+    node.parentElement.parentElement.remove();
 }
