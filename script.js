db5b47036b577cd815a97394a9a49daf9d84cce4
skgough <44104194+skgough@users.noreply.github.com>
2021-09-20 15:31:39 -0400

    lots of improvements
    added export to html file to template system.
    removed print function in template system.
    add print function to recipes.
    refine step-by-step ui with a scroll-snapping based implementation; remove swipe.js dependency.

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-let slider = document.querySelector('.slider');
-var steps;
+function showSteps() {
+    const ingredientSrc = document.querySelector('.ingredients')
+    const stepSrc = document.querySelectorAll('.preparation li')
 
-steps = new Swipe(slider, {
-    startSlide: 0,
-    draggable: true,
-    autoRestart: false,
-    continuous: false,
-    disableScroll: true,
-    stopPropagation: true,
-    callback: function (index) {
-        document.querySelector('.indicator button.active').classList.remove('active');
-        document.querySelectorAll('.indicator button')[index].classList.add('active');
-    }
-});
+    const overlay = document.createElement('div')
+    overlay.classList.add('overlay')
 
-function toggleSteps() {
-    document.body.classList.toggle('overlaid');
-    document.querySelector('.overlay').classList.toggle('underneath');
+    const header = document.createElement('div')
+    header.classList.add('header')
+
+    const steps = document.createElement('div')
+    steps.classList.add('steps')
+
+    const ingredients = document.createElement('div')
+    ingredients.classList.add('ingredients')
+    const grabPane = document.createElement('div')
+    grabPane.classList.add('grab', 'pane')
+    const clearPane = document.createElement('div')
+    clearPane.classList.add('clear', 'pane')
+
+    header.innerHTML = document.querySelector('[onclick="showSteps()"]').innerHTML
+    header.innerHTML += 
+    `<button title="Close" class='close' onclick='closeStepByStep()'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <defs/>
+            <path fill="black" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+        </svg>
+    </button>`
+
+    stepSrc.forEach(src => {
+        const step = document.createElement('div')
+        step.innerText = src.innerText
+        steps.appendChild(step)
+    })
+
+    grabPane.innerHTML =
+        `<div class='knob'><div></div></div>
+      <h2>Ingredients</h2>
+      <div class='table-container'>${ingredientSrc.outerHTML}</div>
+    `
+    ingredients.appendChild(clearPane)
+    ingredients.appendChild(grabPane)
+
+    overlay.appendChild(header)
+    overlay.appendChild(steps)
+    overlay.appendChild(ingredients)
+
+    document.body.appendChild(overlay)
+    document.body.classList.add('overlaid')
 }
-
-function toggleIngredients() {
-    let ingredients = document.querySelector('.overlay .ingredients');
-    ingredients.classList.toggle('reveal');
+function closeStepByStep() {
+    document.querySelector('.overlay').remove()
+    document.body.classList.remove('overlaid')
 }
--- template.js
+++ template.js
@@ -56,11 +56,11 @@ async function handleFiles() {
     let file = this.files[0];
     let figure = document.createElement('figure');
     figure.innerHTML = `<button title="Delete" class='delete' onclick='deleteImage(this)'>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                    <defs/>
-                                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-                                </svg>
-                            </button>`
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <defs/>
+                                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                            </svg>
+                        </button>`
     let img = document.createElement('img');
 
     img.src = await toBase64(file);
@@ -115,13 +115,13 @@ function deleteImage(node) {
     node.parentElement.replaceWith(button);
 }
 function numToString(integer) {
-    let digitsToTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let digitsToTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
     let tensPlace = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    if (integer < 20) {
+    if (integer <= 20) {
         return digitsToTwenty[integer]
     }
-    if (integer >= 20) {
-        return tensPlace[Math.floor(integer / 10)] + ' ' + digitsToTwenty[integer - Math.floor(integer / 10) * 10]
+    if (integer > 20) {
+        return tensPlace[Math.floor(integer / 10)] + '-' + digitsToTwenty[integer - Math.floor(integer / 10) * 10]
     }
 }
 const toBase64 = file => new Promise((resolve, reject) => {
@@ -131,108 +131,67 @@ const toBase64 = file => new Promise((resolve, reject) => {
     reader.onerror = error => reject(error);
 });
 
-async function publish() {
-    await removeTemplateFuntionality();
+async function saveOfflineCopy() {
+    await removeTemplateFunctionality();
 
     let ingrHeader = document.querySelector('.ingredients-header');
     let stepByStep = document.createElement('button');
-    stepByStep.setAttribute("onclick", "toggleSteps()");
+    stepByStep.setAttribute("onclick", "showSteps()");
     stepByStep.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                 <defs />
                                 <path
                                     d="M209.192 241h-91.34c-4.6 0-8.945 2.11-11.789 5.725s-3.873 8.334-2.79 12.805c8.586 35.465 22.459 71.72 60.249 71.72s51.662-36.255 60.249-71.72c1.083-4.47.054-9.19-2.79-12.805S213.792 241 209.192 241zM163.522 0c-20.128 0-37.859 8.034-51.276 23.233-24.555 27.816-32.7 76.349-24.208 144.251 1.423 11.36 2.75 21.303 4.056 30.398 1.061 7.385 7.387 12.868 14.848 12.868h113.16c7.46 0 13.787-5.483 14.848-12.868 1.311-9.123 2.637-19.066 4.056-30.398v-.002c8.491-67.9.346-116.432-24.208-144.249C201.381 8.034 183.651 0 163.522 0zM394.147 421.75h-91.34c-4.6 0-8.945 2.11-11.789 5.725s-3.873 8.334-2.79 12.805c8.586 35.465 22.459 71.72 60.249 71.72s51.662-36.255 60.249-71.72c1.083-4.47.054-9.19-2.79-12.805s-7.189-5.725-11.789-5.725zM399.753 203.983c-13.417-15.199-31.147-23.233-51.276-23.233s-37.859 8.034-51.276 23.233c-24.555 27.816-32.7 76.349-24.208 144.251 1.419 11.332 2.746 21.276 4.056 30.398 1.061 7.385 7.387 12.868 14.848 12.868h113.16c7.46 0 13.787-5.483 14.848-12.868 1.306-9.095 2.633-19.038 4.056-30.398v-.003c8.492-67.899.347-116.432-24.208-144.248z" />
                             </svg>
-                            <div>Step-by-step</div>`;
+                            <span>Step-by-step</span>`;
     ingrHeader.appendChild(stepByStep);
 
-    let overlay = document.createElement('div');
-    overlay.classList.add('overlay', 'underneath');
+    let printBtn = document.createElement('button');
+    printBtn.setAttribute('onclick', 'print()')
+    printBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                              <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"/>
+                          </svg><span>Print</span>`
+    ingrHeader.appendChild(printBtn);
 
-    let tableSrc = document.querySelector('table.ingredients');
-    let table = tableSrc.cloneNode(true);
+    // let indicator = document.createElement('div');
+    // indicator.classList.add('indicator');
+    // let numSteps = slides.querySelectorAll('li').length;
+    // for (i = 0; i < numSteps; i++) {
+    //     let button = document.createElement('button');
+    //     button.setAttribute('onclick', `steps.slide(${i})`);
+    //     button.innerHTML = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    //                             <circle cx="50" cy="50" r="50"/>
+    //                         </svg>`;
+    //     indicator.appendChild(button);
+    // }
+    // indicator.querySelectorAll('button')[0].classList.add('active');
 
-    let dropDown = document.createElement('button');
-    dropDown.classList.add('drop-down');
-    dropDown.setAttribute('onclick','toggleIngredients()');
-    dropDown.innerHTML = `
-        <span>Ingredients</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/>
-        </svg>`;
+    // let exit = document.createElement('button');
+    //     exit.classList.add('exit-steps');
+    // exit.innerHTML = `
+    //     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    //         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+    //     </svg>`;
+    // exit.setAttribute('onclick','showSteps()');
 
-    let slider = document.createElement('div');
-    slider.classList.add('slider');
-    let slides = document.createElement('ol');
-    let lis = document.querySelectorAll('ol.preparation li');
-    for (i=0;i<lis.length;i++) {
-        let li = document.createElement('li');
-        let div = document.createElement('div');
-            div.innerText = lis[i].innerText;
-        li.innerHTML = div.outerHTML;
-        slides.appendChild(li);
-    }
-    slider.appendChild(slides);
-
-    let indicator = document.createElement('div');
-    indicator.classList.add('indicator');
-    let numSteps = slides.querySelectorAll('li').length;
-    for (i = 0; i < numSteps; i++) {
-        let button = document.createElement('button');
-        button.setAttribute('onclick', `steps.slide(${i})`);
-        button.innerHTML = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="50" cy="50" r="50"/>
-                            </svg>`;
-        indicator.appendChild(button);
-    }
-    indicator.querySelectorAll('button')[0].classList.add('active');
-
-    let exit = document.createElement('button');
-        exit.classList.add('exit-steps');
-    exit.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-        </svg>`;
-    exit.setAttribute('onclick','toggleSteps()');
-
-    overlay.appendChild(table);
-    overlay.appendChild(dropDown);
-    overlay.appendChild(slider);
-    overlay.appendChild(indicator);
-    overlay.appendChild(exit);
-
-    document.body.appendChild(overlay);
-
-    let swipe = document.createElement('script');
-        swipe.src= "swipe.min.js";
     let script = document.createElement('script');
         script.src= "script.js";
         script.setAttribute('defer',true);
-    document.head.appendChild(swipe);
     document.head.appendChild(script);
     
     document.head.querySelector('script[src="template.js"]').remove();
+
+    const htmlContent = [document.documentElement.outerHTML];
+    let bl = new Blob(htmlContent, {type: "text/html"});
+    let a = document.createElement("a");
+    a.href = URL.createObjectURL(bl);
+    a.download = `${document.title.replace(/[^\w\s]/gi, '')}.html`;
+    a.hidden = true;
+    document.body.appendChild(a);
+    a.innerHTML = "Output";
+    // a.click();
 }
 
-async function toPaper() {
-    await removeTemplateFuntionality();
-    document.querySelector('h1 > a').remove();
-    setTimeout(function () {
-        waitForPaint();
-    }, 200);
-    function waitForPaint() {
-        if (document.readyState == 'complete') {
-            print();
-        } else {
-            document.onreadystatechange = function () {
-                if (document.readyState === "complete") {
-                    print();
-                }
-            }
-        }
-    }
-}
-
-function removeTemplateFuntionality() {
+function removeTemplateFunctionality() {
     return new Promise((resolve) => {
         document.querySelector('input').remove();
         document.head.querySelector('link').href = "style.css";
