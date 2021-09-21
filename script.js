71ec30b886b824358bad0ee0805a92724c94a932
skgough <44104194+skgough@users.noreply.github.com>
2021-09-20 22:41:39 -0400

    start pwa

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
+  
+      if(reg.installing) {
+        console.log('Service worker installing');
+      } else if(reg.waiting) {
+        console.log('Service worker installed');
+      } else if(reg.active) {
+        console.log('Service worker active');
+      }
+  
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  }
+
 function showSteps() {
     const ingredientSrc = document.querySelector('.ingredients')
     const stepSrc = document.querySelectorAll('.preparation li')
