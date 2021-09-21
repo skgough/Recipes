f70e186bd95afb7d3a33d285a781e168a341d618
skgough <44104194+skgough@users.noreply.github.com>
2021-09-20 22:44:31 -0400

    fix sw

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
+   
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
-  
-      if(reg.installing) {
-        console.log('Service worker installing');
-      } else if(reg.waiting) {
-        console.log('Service worker installed');
-      } else if(reg.active) {
-        console.log('Service worker active');
-      }
-  
-    }).catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-  }
+    navigator.serviceWorker
+        .register('sw.js')
+        .then(() => { console.log('Service Worker Registered') })
+}
 
 function showSteps() {
     const ingredientSrc = document.querySelector('.ingredients')
--- sw.js
+++ sw.js
@@ -1,32 +1,17 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('cache').then(function(cache) {
-        return cache.addAll([
-          'index.html',
-          'style.css'
-        ]);
-      })
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('recipes').then((cache) => cache.addAll([
+            'index.html',
+            'style.css',
+            'script.js'
+        ])),
     );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open('cache').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        })
-      }
-    }));
-  });
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
