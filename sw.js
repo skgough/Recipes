25ec95768a8b7dbc01cee27b39adcd4df8a1be4c
skgough <44104194+skgough@users.noreply.github.com>
2021-09-20 23:52:30 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('recipes').then((cache) => cache.addAll([
-            'index.html',
             'style.css',
             'script.js'
         ])),
