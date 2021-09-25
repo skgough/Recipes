bb125feb80cf3d2c8621a447850aee9c1a28deb3
skgough <44104194+skgough@users.noreply.github.com>
2021-09-24 23:12:21 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('recipes').then((cache) => cache.addAll([
-            'reset.css',
-        ])),
-    );
-});
-
-self.addEventListener('fetch', (e) => {
-    console.log(e.request.url);
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open('mysite-dynamic').then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return (
+                    response ||
+                    fetch(event.request).then(function (response) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                );
+            });
+        }),
     );
 });
